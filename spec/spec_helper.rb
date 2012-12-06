f98498a54ba2e079eca0ece0e97739fa768b6e73
Everton J. Carpes <everton.carpes@gmail.com>
2012-12-06 15:59:35 -0200

    Adding fake responses of Correios webservice for current tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
 
+require_relative 'support/fakeweb'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
